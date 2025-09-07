18b4b179e09f18f3fe0bdd9e362f17b23313f25c
shivvooo <shivaramprasadh01@gmail.com>
2025-09-07 10:46:14 +0530

    divRef added

--- src/info.js
+++ src/info.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 
 function Info(){
 
+      let [product,setproduct] = useState({veggies:"",milk:"",meat:""})
+      const divRef = useRef(null);
+      if (divRef.current) {
+      divRef.current.style.backgroundColor = "lightblue";
+    divRef.current.style.width = "100%";
+    divRef.current.style.padding = "30px";  
+    }
+  product = [{
+
+    veggies: "carrot",
+    milk: "1ltr",
+    meat: "chicken"
+  },
+   {
+    veggies:"beets",
+    milk: "1/2 ltr",
+    meat:"fish"
+   }
+]
     return (
 
         <div>
-
-            welcome
+            <div ref = {divRef}>
+<table border="1">
+    <thead>
+        <tr>
+        <th>veggies</th>
+        <th>milk</th>
+        <th>meat</th>
+    </tr>
+    </thead>
+    <tbody>
+        {product.map((products,index) => (
+     <tr key = {index}>
+        <td>
+{products.veggies}
+        </td>
+        <td>
+            {products.milk}
+        </td>
+        <td>
+            {products.meat}
+        </td>
+        
+        </tr>
+))}
+    </tbody>
+</table>
 
         </div>
+    </div>
     )
 }
 
